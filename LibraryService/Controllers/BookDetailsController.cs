db394dbb32a697629cb499f1e2c3e3e9e907d1fd
kanwal019 <kanwal019@gmail.com>
2021-01-07 19:16:33 +0530

    Initial Clean Up

---
+++ LibraryService/Controllers/BookDetailsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookDetailsController : ControllerBase
+    {
+        private readonly ILogger<BookDetailsController> _logger;
+
+        public BookDetailsController(ILogger<BookDetailsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> FetchAllBooks()
+        {
+            try
+            {
+                return "All Books";
+            }
+            catch (Exception ex)
+            {
+                _ = ex;
+                return ex.Message;
+            }
+        }
+    }
+}

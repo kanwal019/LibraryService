ac3f90631216a317e82f485af4e74ea488ca8a57
kanwal019 <kanwal019@gmail.com>
2021-01-07 19:31:08 +0530

    Added a basic skeleton

--- LibraryService/Controllers/BookDetailsController.cs
+++ LibraryService/Controllers/BookDetailsController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LibraryService.Controllers
+namespace Library.Service.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
